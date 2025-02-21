2ebd706781f57aff9655c04b01dfe7f950ee278e
Takashi SAKAGUCHI <hamajyotan@gmail.com>
2025-02-22 02:58:02 +0900

    add #persisted? definition and comment.

--- lib/active_record_compose/model.rb
+++ lib/active_record_compose/model.rb
@@ -258,6 +258,15 @@ module ActiveRecordCompose
       end || raise_on_save_error
     end
 
+    # Returns true if model is persisted.
+    #
+    # By overriding this definition, you can control the callbacks that are triggered when a save is made.
+    # For example, returning false will trigger before_create, around_create and after_create,
+    # and returning true will trigger before_update, around_update and after_update.
+    #
+    # @return [Boolean] returns true if model is persisted.
+    def persisted? = super
+
     private
 
     def models = @__models ||= ActiveRecordCompose::ComposedCollection.new(self)
