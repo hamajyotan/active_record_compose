431ea46c934c6126d0fb696e10c9c61551c47af4
Takashi SAKAGUCHI <hamajyotan@gmail.com>
2025-05-30 00:19:40 +0900

    Add test based on ActiveMode::Lint::Tests

---
+++ test/active_record_compose/model_lint_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'active_record_compose/model'
+
+class ActiveRecordCompose::ModelLintTest < ActiveSupport::TestCase
+  include ActiveModel::Lint::Tests
+
+  class ComposedModel < ActiveRecordCompose::Model
+    def initialize(account = Account.new)
+      @account = account
+      @profile = account.then { _1.profile || _1.build_profile }
+      super()
+      models << account << profile
+    end
+
+    delegate_attribute :name, :email, to: :account
+    delegate_attribute :firstname, :lastname, :age, to: :profile
+
+    private
+
+    attr_reader :account, :profile
+  end
+
+  setup do
+    @model = ComposedModel.new
+  end
+end
